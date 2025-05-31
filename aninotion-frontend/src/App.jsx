571a70f771571a897bafdc994b37f6afb6de189f
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2025-05-31 14:10:29 +0000

    feat: restructure App component and add Home page

--- aninotion-frontend/src/App.jsx
+++ aninotion-frontend/src/App.jsx
@@ -1,16 +1,21 @@
-
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './pages/Home';
 
 function App() {
-  
-
   return (
-    <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        hello world
-      </div>
-    </>
-  )
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/anime" element={<div>Anime Page</div>} />
+          <Route path="/manga" element={<div>Manga Page</div>} />
+          <Route path="/other" element={<div>Other Page</div>} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
-export default App
+export default App;
