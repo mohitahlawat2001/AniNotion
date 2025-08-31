bd2e25c64bac4fb16beeac42610ebfdc92019355
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2025-08-31 17:05:42 +0000

    feat: Add Vercel configuration and redirects for SPA routing

--- aninotion-frontend/vite.config.js
+++ aninotion-frontend/vite.config.js
@@ -4,5 +4,18 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,
+    open: true,
+    historyApiFallback: {
+      index: '/index.html',
+    },
+  },
+  preview: {
+    port: 4173,
+    historyApiFallback: {
+      index: '/index.html',
+    },
+  },
 })
