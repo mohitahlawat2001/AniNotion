4631841191cc5ca79ef6165c2c92a90ad0bcdb38
candykisu <uraharakisu@proton.me>
2025-10-10 23:59:18 +0530

    feat: added husky

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'prefer-const': 'error',
+    'no-var': 'error',
+  },
+  overrides: [
+    {
+      files: ['aninotion-frontend/**/*.{js,jsx}'],
+      env: {
+        browser: true,
+        es2021: true,
+      },
+      extends: ['eslint:recommended'],
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    {
+      files: ['aninotion-backend/**/*.js'],
+      env: {
+        node: true,
+        es2021: true,
+      },
+      extends: ['eslint:recommended'],
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+  ],
+};
